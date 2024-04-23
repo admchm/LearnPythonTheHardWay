c16a2fb811c2fe555307332506da7fcaecfd2e1d
Adam Chomicki <admchm@outlook.com>
2024-04-23 14:39:57 +0200

    Add code for scraping data chapter

---
+++ subprojects/52_Scraping_Data_from_the_Web/ex52.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+from urllib import request
+import os
+
+ttb_url = "https://web.archive.org/web/20230906095156/https://www.ttb.gov/beer/statistics"
+print(ttb_url)
+
+if not os.path.exists("ttb_stats.json"):
+    with open("ttb_page.html", "wb") as f:
+        resp = request.urlopen(ttb_url)
+        body = resp.read()
+        f.write(body)
+else:
+    with open("ttb_page.html") as f:
+        body = f.read()
+
+# change this to lxml instead of html5lib if you can't use it
+soup = BeautifulSoup(body, "lxml")
+print(soup.title)
+
+
