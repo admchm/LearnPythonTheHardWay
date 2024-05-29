a7f5ff8addbcf861a2ff16203dd74a645ad962b0
Adam Chomicki <admchm@outlook.com>
2024-05-29 10:24:07 +0200

    Add the code for ex12 - An easier way to prompt

---
+++ subprojects/12_An_Easier_Way_to_Prompt/ex12.py
@@ -0,0 +1,11 @@
+y = input("Name? ")
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy")
+
+# Search in Google:
+# python print site:python.org
+# https://docs.python.org/3/tutorial/inputoutput.html
