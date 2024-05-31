a28958de3e9e8824befba325ad2bbfa5d2f16f0b
Adam Chomicki <admchm@outlook.com>
2024-05-31 10:38:17 +0200

    Finish exercises in ex14.py

--- subprojects/14_Prompting_and_Passing/ex14.py
+++ subprojects/14_Prompting_and_Passing/ex14.py
@@ -19,3 +19,52 @@ Alright, so you said {likes} about liking me.
 You live in {lives}.  Not sure where that is.
 And you have a {computer} computer.  Nice.
 """)
+
+## Study Drills:
+
+## 2 - Change the prompt variable to something else entire
+
+# Answer: 
+# script, user_name = argv
+# prompt = '$ '
+
+# print(f"Hi {user_name}, I'm the {script} script.")
+# print("I'd like to ask you a few questions.")
+# print(f"Do you like me {user_name}?")
+# likes = input(prompt)
+
+# print(f"Where do you live {user_name}?")
+# lives = input(prompt)
+
+# print("What kind of computer do you have?")
+# computer = input(prompt)
+
+# print(f"""
+# Alright, so you said {likes} about liking me.
+# You live in {lives}.  Not sure where that is.
+# And you have a {computer} computer.  Nice.
+# """)
+
+## 2 - Add another argument and use it in your script, the same way you did in the 
+## previous exercise with first, second = ARGV.
+
+# Answer: 
+# script, user_name, age = argv
+# prompt = '$ '
+
+# print(f"Hi {user_name}, you're {age} years old. I'm the {script} script.")
+# print("I'd like to ask you a few questions.")
+# print(f"Do you like me {user_name}?")
+# likes = input(prompt)
+
+# print(f"Where do you live {user_name}?")
+# lives = input(prompt)
+
+# print("What kind of computer do you have?")
+# computer = input(prompt)
+
+# print(f"""
+# Alright, so you said {likes} about liking me.
+# You live in {lives}.  Not sure where that is.
+# And you have a {computer} computer.  Nice.
+# """)
