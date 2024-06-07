f9aa9e9b4b65b7f3c5569abd53970646eede3ca4
Adam Chomicki <admchm@outlook.com>
2024-06-07 15:44:36 +0200

    Finish exercises for chapter 22 - Strings, Bytes and Character Encodings

--- subprojects/22_Strings_Bytes_and_Character_Encodings/study_drill_#2.py
+++ subprojects/22_Strings_Bytes_and_Character_Encodings/study_drill_#2.py
@@ -2,3 +2,5 @@
 ## Find out what happens when you give an encoding that doesn't exist.
 
 # Answer: 
+# It seems like it is not being encoded, but it doesn't give any errors
+# b'Some Non Existing Language' <===> Some Non Existing Language
--- subprojects/22_Strings_Bytes_and_Character_Encodings/study_drill_#3.py
+++ subprojects/22_Strings_Bytes_and_Character_Encodings/study_drill_#3.py
@@ -2,4 +2,7 @@
 ## Can you break the encodings by damaging the file? How much do you need to remove to cause Python to break? How 
 ## much can you remove to damage the string output, but pass Python's decoding system?
 
-# Answer: 
+# Answer: To corrupt the file, we could open languages.txt in a binary editor (or use a programming 
+# tool to manipulate it programmatically). Next, change or delete some bytes in the middle of 
+# a multi-byte character. For instance, in UTF-8, characters outside the basic ASCII set are encoded
+# using multiple bytes (2-4 bytes). Altering one of these bytes can make the sequence invalid.
