3ae08506a6836cfd8e1e260696ab625077ef1e06
Adam Chomicki <admchm@outlook.com>
2024-06-07 15:05:39 +0200

    Add comments and a link to a documentation

--- subprojects/22_Strings_Bytes_and_Character_Encodings/ex22.py
+++ subprojects/22_Strings_Bytes_and_Character_Encodings/ex22.py
@@ -10,8 +10,8 @@ def main(language_file, encoding, errors):
     
 def print_line(line, encoding, errors):
     next_lang = line.strip()
-    raw_bytes = next_lang.encode(encoding, errors=errors)
-    cooked_string = raw_bytes.decode(encoding, errors=errors)
+    raw_bytes = next_lang.encode(encoding, errors=errors)     # creating coded string 
+    cooked_string = raw_bytes.decode(encoding, errors=errors) # recoding coded string. Could also just use print(next_lang)
     
     print(raw_bytes, "<===>", cooked_string)
     
@@ -21,8 +21,11 @@ main(languages, input_encoding, error)
 
 #python3 -m pydoc io.IOBase.seek
 #python3 -m pydoc str.strip
+#python3 -m pydoc str.encode
+
+#https://docs.python.org/3/library/stdtypes.html#bytes.decode
 
 # calling the script:
-#python ex22.py "utf-8" "strict"
-#python ex22.py "utf-8" "ignore"
-#python ex22.py "utf-8" "replace"
+#python3 ex22.py "utf-8" "strict"
+#python3 ex22.py "utf-16" "ignore"
+#python3 ex22.py "big5" "replace"
