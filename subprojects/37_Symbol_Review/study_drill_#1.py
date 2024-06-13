5f64146e657edffd940631e49a4cdeac5db7713e
Adam Chomicki <admchm@outlook.com>
2024-06-13 15:27:48 +0200

    Finish study drills for chapter 37

--- subprojects/37_Symbol_Review/study_drill_#1.py
+++ subprojects/37_Symbol_Review/study_drill_#1.py
@@ -2,3 +2,42 @@
 ## Find out what a "flow chart" is and draw a few.
 
 # Answer: 
+
+# +-----------------------+
+# | Start                 |
+# +-----------------------+
+#             |
+#             v
+# +-----------------------+
+# | Input a number        |
+# +-----------------------+
+#             |
+#             v
+# +-----------------------+
+# | Is the number         |
+# | divisible by 2?       |
+# +-----------------------+
+#       /     \
+#      v       v
+# +--------+ +--------+
+# | Yes    | | No     |
+# +--------+ +--------+
+#       |       |
+#       v       v
+# +-----------------------+   +-----------------------+
+# | Print "The number     |   | Print "The number     |
+# | is even"              |   | is odd"               |
+# +-----------------------+   +-----------------------+
+#             |                       |
+#             v                       v
+# +-----------------------+
+# | End                   |
+# +-----------------------+
+
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:  # Is the number divisible by 2?
+    print("The number is even")
+else:
+    print("The number is odd")
--- subprojects/37_Symbol_Review/study_drill_#3.py
+++ subprojects/37_Symbol_Review/study_drill_#3.py
@@ -1,4 +1,6 @@
 ## Study Drill #3: 
-## 
+## Another technique for when you are not using paper is to put # 
+## comments with your notes in the code. Sometimes, these could 
+## become the actual comments to help the next person
 
-# Answer: 
+# Answer: ---
