e538788715d91929c4b81315244c65ff0afb8e42
Adam Chomicki <admchm@outlook.com>
2024-06-19 11:15:52 +0200

    Fix mistakes by transferring the code from files related to the chapter 43 to 44

--- subprojects/44_From_Dictionaries_to_Objects/study_drill_#1.py
+++ subprojects/44_From_Dictionaries_to_Objects/study_drill_#1.py
@@ -1,4 +1,21 @@
 ## Study Drill #1: 
-## 
+## Use Person_new to create a few more people.
 
-# Answer: ---
+# Answer:
+
+def Person_new(name, age, eyes):
+    person = {
+        "name": name,
+        "age": age,
+        "eyes": eyes
+    }
+    
+    def talk(words):
+        print(f"I am {person['name']} and {words}")
+        
+    person["talk"] = talk
+    
+    return person
+
+mark = Person_new("mark", 47, "brown")
+mark["talk"]("I'm shouting right now!")
