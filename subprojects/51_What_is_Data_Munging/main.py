c6e6655484b5d2a0c92294280fecae2ffd6fda2d
Adam Chomicki <admchm@outlook.com>
2024-07-02 12:47:22 +0200

    Rename variables; Get production data

--- subprojects/51_What_is_Data_Munging/main.py
+++ subprojects/51_What_is_Data_Munging/main.py
@@ -22,10 +22,10 @@ def get_report_date(lines):
             return line
         
 def populate_columns(lines):
-    current_month = []
-    prior_year_current_month = []
-    current_year_cumulative_to_date = []
-    prior_year_cumulative_to_date = []
+    first_col = []
+    second_col = []
+    third_col = []
+    fourth_col = []
     
     numbers = re.compile(r"^[,\d\s]+$")
 
@@ -33,25 +33,30 @@ def populate_columns(lines):
     for line in lines:
         line = numbers.match(line)
         
-        first_column = 9
-        second_column = 18
-        third_column = 27
-        fourth_column = 36
+        first_column_last_element = 9
+        second_column_last_element = 18
+        third_column_last_element = 27
+        fourth_column_last_element = 36
         
         if line != None:
-            if i < first_column:
-                current_month.append(line.string)
+            if i < first_column_last_element:
+                first_col.append(line.string)
                 
-            if i < second_column and i > first_column:
-                prior_year_current_month.append(line.string)
+            if i < second_column_last_element and i > first_column_last_element:
+                second_col.append(line.string)
                 
-            if i < third_column and i > second_column:
-                current_year_cumulative_to_date.append(line.string)
+            if i < third_column_last_element and i > second_column_last_element:
+                third_col.append(line.string)
                 
-            if i < fourth_column and i > third_column:
-                prior_year_cumulative_to_date.append(line.string)
+            if i < fourth_column_last_element and i > third_column_last_element:
+                fourth_col.append(line.string)
                 
             i += 1
+    
+    return first_col, second_col, third_col, fourth_col
+
+def get_production(first_col, second_col, third_col, fourth_col):
+    return first_col[0], second_col[0], third_col[0], fourth_col[0]
     
 file_name = "Statistical_Report_Beer_October_2021.pdf"
 file = open(file_name, "rb")
@@ -60,10 +65,16 @@ pdf = pdftotext.PDF(file)
 
 lines = "".join(pdf).split("\n")
 
-reporting_period = get_reporting_period(lines)
-print(reporting_period)
+current_month, prior_year_current_month, current_year_cumulative_to_date, prior_year_cumulative_to_date = populate_columns(lines)
 
-report_date = get_report_date(lines)
-print(report_date)
+print("\n#################### ANSWERS #################### ")
+print(f"# - Reporting Period: {get_reporting_period(lines)}")
+print(f"# - Report Date: {get_report_date(lines)}")
+print(f"# - Production for Current Month, Prior Year, Cumulative to Date:")
+print(get_production(current_month, prior_year_current_month, current_year_cumulative_to_date, prior_year_cumulative_to_date))
+# - Stocks on Hand End of Month for Current Month, Prior Year Current Month
+# - Calculate the difference between Production and Stock on Hand 
+# - End-of-Month to determine the actual sales that month.
 
-populate_columns(lines)
+
+
