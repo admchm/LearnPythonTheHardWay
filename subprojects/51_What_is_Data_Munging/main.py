8ce9e37e6d1e6e1b73e46883498109cbb76ae2d2
Adam Chomicki <admchm@outlook.com>
2024-07-02 11:12:22 +0200

    Get reporting period

--- subprojects/51_What_is_Data_Munging/main.py
+++ subprojects/51_What_is_Data_Munging/main.py
@@ -1,7 +1,20 @@
 # https://github.com/jalan/pdftotext - instructions
 
+# Your job is to download that PDF and extract the following data:
+# - Reporting Period
+# - Report Date
+# - Production for Current Month, Prior Year, Cumulative to Date
+# - Stocks on Hand End of Month for Current Month, Prior Year Current Month
+# - Calculate the difference between Production and Stock on Hand 
+#   End-of-Month to determine the actual sales that month.
+
 import pdftotext
 
+def get_reporting_period(lines):
+    for line in lines:
+        if line.startswith("Reporting Period"):
+            return line
+    
 file_name = "Statistical_Report_Beer_October_2021.pdf"
 file = open(file_name, "rb")
 
@@ -9,15 +22,13 @@ pdf = pdftotext.PDF(file)
 
 lines = "".join(pdf).split("\n")
 
-i = 0
-for line in lines:
-    i += 1
-    print(f"#{i}: {line}")
+reporting_period = get_reporting_period(lines)
+print(reporting_period)
 
-# find Reporting Period
+# i = 0
 # for line in lines:
-#     if line.startswith("Reporting Period"):
-#         # print it
-#         print(line)
-#     else:
-#         print(line)
+#     i += 1
+#     print(f"#{i}: {line}")
+    
+
+
